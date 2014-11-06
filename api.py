8aa0761bc7f826462b001f45f99a05b307bcbb51
Miguel Laginha <mmachado@ipn.pt>
2014-11-06 08:58:05 +0000

    postprocessor for pushing the tweet to followers minor tweaks for the red button simulator

--- api.py
+++ api.py
@@ -3,15 +3,30 @@ from  flask.ext.restless import APIManager
 from app import app
 from models import User, Message, UserFollow, db
 
+# postprocessors
+def post_post_message(result, **kw): 
+    author = User.query.filter_by(id=result.get('author_id')).first() 
+    # debug
+    print "The tweet saying '%s' is going to be pushed to the following users: %r" % (result.get('text'), author.followed_by_list)
+    #TODO
+
 # Create the Flask-Restless API manager.
 manager = APIManager(app, flask_sqlalchemy_db=db)
 
 # Create API endpoints, which will be available at /api/<tablename> by
 # default. Allowed HTTP methods can be specified as well.
 includes = ['id', 'username', 'email', 'followed_by_list', 'followed_by_list.followed_by_id', 'followed_list', 'followed_list.followed_id']
-manager.create_api(User, methods=['GET', 'POST', 'DELETE'], collection_name='people', include_columns=includes)
+manager.create_api(User, methods=['GET', 'POST', 'DELETE'], 
+    collection_name='people', 
+    include_columns=includes)
 
-# manager.create_api(UserFollow, methods=['GET', 'POST'], collection_name='followers', exclude_columns=['pw_hash'])
+# manager.create_api(UserFollow, methods=['GET', 'POST'],
+    # collection_name='followers',
+    # exclude_columns=['pw_hash'])
 
 includes = ['message_id', 'author_id', 'text']
-manager.create_api(Message, methods=['GET', 'POST'], collection_name='messages')
+manager.create_api(Message, methods=['GET', 'POST'], 
+    collection_name='messages',
+    postprocessors={
+        'POST': [post_post_message]
+    })
--- red_button_simulator.py
+++ red_button_simulator.py
@@ -8,9 +8,13 @@ now = str(datetime.now())
 url = 'http://localhost:5000/api/messages'
 # payload = {"author_id": 1, "text": "help!", "pub_date": now}
 payload = {"author_id": 1, "text": "help!"}
-print payload
+# print payload
 headers = {'content-type': 'application/json'}
-r = requests.post(url, data=json.dumps(payload), headers=headers)
+response = requests.post(url, data=json.dumps(payload), headers=headers)
 
-print r.url
-print r.text
+assert response.status_code == 201
+print(response.json())
+
+# debug
+# print r.url
+# print r.text
