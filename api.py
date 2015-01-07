845ec364a3b63880dc869434e0f2b17e60f9af4f
Miguel Laginha <mmachado@ipn.pt>
2015-01-07 20:45:58 +0000

    now using TO notifications instead of simple flat feeds now using Google distance API to sort out the driving distance between care seekers and carers

--- api.py
+++ api.py
@@ -8,10 +8,12 @@ from flask.ext.restless import ProcessingException
 from authentication import check_auth
 from flask import request, abort
 import json
+import requests
+import pprint
 
 # preprocessor
 def auth_func(*args, **kwargs):
-    print "DEBUG: request.authorization, ", request.authorization
+    # print "DEBUG: request.authorization, ", request.authorization
     if not check_auth(request):
         # return Response(status=401)
         raise ProcessingException(description='Not authenticated!', code=403)
@@ -29,17 +31,62 @@ def replace_username_for_author_id(data=None, **kw):
 def post_create_message(result, **kw):
     # get the feed for the message author
     author_id = result.get('author_id')
-    print "author_id: ", author_id
+    author = User.query.filter_by(id=author_id).first()
+    author_location = author.current_location
     user_feed = client.feed('user:'+str(author_id))
-    activity_data = {'actor': author_id, 'verb': 'tweet', 'object': result.get('message_id'), 'tweet': result.get('text')}
-    activity_response = user_feed.add_activity(activity_data)
-
+    tweet = result.get('text')
+    object = result.get('message_id')
+    
+    # list of the given feed's followers
+    author_followers = user_feed.followers(offset=0, limit=10)
+    
+    # debug printing
+    # print "%s (from %s) is sending messages..." % (author.full_name, author_location)
+    
+    # send activity individually because of the costumized message
+    for each_follower in author_followers.get('results'):
+        
+        # get the data from each follower
+        follower_feed = each_follower.get('feed_id')
+        follower_id = follower_feed.split(':')[-1]
+        follower = User.query.filter_by(id=follower_id).first()
+        follower_location = follower.current_location
+        
+        # use google distance API to get driving distance between care seeker and carer
+        key = ''
+        response = requests.get('https://maps.googleapis.com/maps/api/distancematrix/json?key=' + key + '+&origins=' + follower_location +'&destinations=' + author_location)
+        data = response.json()
+        # pp = pprint.PrettyPrinter(indent=4)
+        # pp.pprint(data)
+        if data.get('status') == 'OK':
+            duration = data.get('rows')[0].get('elements')[0].get('duration')
+            print "====> %s would take %s to rescue %s" % (follower.full_name, duration.get('text'), author.full_name)
+        else:
+            duration = -1
+            print "Cannot contact Google to calculate distance... moving on with -1"
+        
+        followers = []
+        followers.append(follower_feed)
+        activity = {
+            'actor': author_id, 
+            'verb': 'tweet', 
+            'object': object, 
+            'tweet': tweet, 
+            'geolocation':author.current_location,
+            'duration': duration.get('text'),
+            'to':followers
+        }
+        
+        # add specific "TO" notifications, one by one
+        activity_response = user_feed.add_activity(activity)
+        # print "Posting feedback: ", activity_response
+    
 # Create the Flask-Restless API manager.
 manager = APIManager(app, flask_sqlalchemy_db=db)
 
 # Create API endpoints, which will be available at /api/<tablename> by
 # default. Allowed HTTP methods can be specified as well.
-includes = ['id', 'username', 'email', 'followed_by_list', 'followed_by_list.followed_by_id', 'followed_list', 'followed_list.followed_id']
+includes = ['id', 'username', 'email', 'full_name', 'followed_by_list', 'followed_by_list.followed_by_id', 'followed_list', 'followed_list.followed_id', 'current_location', 'avatar', 'care_type']
 manager.create_api(User, methods=['GET'],
     collection_name='people',
     include_columns=includes,
@@ -50,7 +97,7 @@ manager.create_api(User, methods=['GET'],
             POST=[auth_func],
             DELETE=[auth_func]))
 
-includes = ['message_id', 'author_id', 'text']
+includes = ['message_id', 'author_id', 'text', 'pub_date']
 manager.create_api(Message, methods=['POST'],
     collection_name='messages',
     postprocessors={
